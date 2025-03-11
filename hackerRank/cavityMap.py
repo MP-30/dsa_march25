ebee5ac19c3886aafc0d853bd36c3cfb034ace2f
mp-30 <adityabhadauriya93@gmail.com>
2025-03-12 00:41:50 +0530

    cavityMap

---
+++ hackerRank/cavityMap.py
@@ -0,0 +1,15 @@
+grid = ['1112', '1912', '1892', '1234']
+
+def cavityMap(grid):
+    grid_list = [list(row) for row in grid] 
+    # print(grid_list)
+    for i in range(0,len(grid)):
+        for j in range(0,len(grid[i])):
+            if (i != 0 and j !=0 and i != len(grid)-1 and j != len(grid)-1) : 
+                if grid[i-1][j] < grid[i][j] and grid[i][j-1] < grid[i][j] and grid[i+1][j] < grid[i][j] and grid[i][j+1] < grid[i][j]:
+                        grid_list[i][j] = 'X'
+
+    grid = ["".join(row) for row in grid_list]
+    return(grid)
+
+print(cavityMap(grid))
