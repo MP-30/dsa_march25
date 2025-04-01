b432b01fff068d17e9518193077e3ecd11d8e2c6
mp-30 <adityabhadauriya93@gmail.com>
2025-04-01 20:37:13 +0530

    two part of list are completed

--- geekyforgeeky/List/list1.py
+++ geekyforgeeky/List/list1.py
@@ -31,17 +31,95 @@ def find_element(l, n):
 
 
 # Different ways to clear a list
+a = [1,2,3]
+b = [3,4,5]
+c = [4,5,6]
+def clear_list(a,b,c):
+    a.clear()
+    c = []
+    del b[:]
+    print(a)
+    print(b)
+    print(c)
+
 # Reversing a List
+def reverse_list(lt):
+    length = len(lt)//2
+    for i in range(0,length):
+        lt[i], lt[-(i+1)] = lt[-(i+1)], lt[i]
+    print(lt)   
+
+
+
 # Find sum of elements in list
+
+def sum_of_lst(lt):
+    sum = 0
+    for i in lt:
+        sum += i
+    print(sum)
+
+
 # Multiply all numbers in the list
+
+def mul_of_lst(lt):
+    mul = 1
+    for i in lt:
+        mul *= i
+    print(mul)
+
+
 # Find smallest number in a list
+
+def smallst(lt):
+    smallest_num = lt[0]
+    for i in lt:
+        if smallest_num > i:
+            smallest_num = i
+        else:pass
+    print(smallest_num)
+
 # Find largest number in a list
+
+
+def largest(lt):
+    largest_num = lt[0]
+    for i in lt:
+        if largest_num < i:
+            largest_num = i
+        else:pass
+    print(largest_num)
+
+
 # Find second largest number in a list
 
+def second_lsrgest(lt):
+    largest_num = 0
+    second_largest = 0
+    for i in lt:
+        if largest_num < i:
+            largest_num = i
+    for j in lt:
+        if second_largest < j and j != largest_num:
+            second_largest = j
+            
+        else:pass
+    print(largest_num)
+    print(second_largest)
+
+
 if __name__ == '__main__':
-    lst = [1,2,3,4,5,6,7,8,9]
+    lst = [2,3,4,5,6,1,7,8,9]
+    
     # interchange(lst)
     # swaping(lst)
     # find_length(lst)
-    find_element(lst, 4)
+    # find_element(lst, 4)
+    # clear_list(a,b,c)
+    # reverse_list(lst)
+    # sum_of_lst(lst)
+    # mul_of_lst(lst)
+    # smallst(lst)
+    # largest(lst)
+    second_lsrgest(lst)
     pass
--- geekyforgeeky/List/list2.py
+++ geekyforgeeky/List/list2.py
@@ -1,10 +1,125 @@
 # Find N largest elements from a list
+
+def n_largest(n,lst):
+    lst.sort()
+    print(lst[n])
+    print(lst)
+
+
 # Even numbers in a list
+
+def find_even(lst):
+    new_lst = []
+    for i in lst:
+        if i % 2 ==0:
+            new_lst.append(i)
+    print(new_lst)
+
+
+
 # Odd numbers in a List
+def find_odd(lst):
+    new_lst = []
+    for i in lst:
+        if i % 2 !=0:
+            new_lst.append(i)
+    print(new_lst)
+
+
 # Print all even numbers in a range
+
+def even_in_range(a,b,lst):
+    new_lst = []
+    for i in range(a,b+1):
+        if lst[i] %2 == 0:
+            new_lst.append(lst[i])
+    print(new_lst)
+
+
 # Print all odd numbers in a range
+
+
+
+def odd_in_range(a,b,lst):
+    new_lst = []
+    for i in range(a,b+1):
+        if lst[i] %2 != 0:
+            new_lst.append(lst[i])
+    print(new_lst)
+
+
 # Print positive numbers in a list
+
+def print_positive(lst):
+    new_lst = []
+    for i in lst:
+        if i > 0:
+            new_lst.append(i)
+
+    print(new_lst)
+
 # Print negative numbers in a list
+
+
+def print_negative(lst):
+    new_lst = []
+    for i in lst:
+        if i < 0:
+            new_lst.append(i)
+
+    print(new_lst)
+
+
 # Print all positive numbers in a range
+
+
+def all_positive_in_range(a,b,lst):
+    new_lst = []
+    for i in range(a,b+1):
+        if lst[i] > 0:
+            new_lst.append(lst[i])
+    print(new_lst)
+
+
 # Print all negative numbers in a range
+
+def all_negative_in_range(a,b,lst):
+    new_lst = []
+    for i in range(a,b+1):
+        if lst[i] < 0:
+            new_lst.append(lst[i])
+    print(new_lst)
+
+
 # Remove multiple elements from a list
+
+def multiple_element(lst):
+    dict = {}
+    for i in lst:
+        if i in dict:
+            dict[i] +=1
+        else:
+            dict[i] = 1
+
+    new_lst = []
+    for j in dict:
+        new_lst.append(j)
+    print(new_lst)
+    # print(dict)
+
+
+
+
+if __name__ == '__main__':
+    lst = [11,1,1,2,3,4,5,-3,-2,6,7,8,8,8,8,9]
+    # n_largest(5,lst)
+    # find_even(lst)
+    # find_odd(lst)
+    # even_in_range(1,5,lst)
+    # odd_in_range(0,5,lst)
+    # print_positive(lst)
+    # print_negative(lst)
+    # all_positive_in_range(1,10,lst)
+    # all_negative_in_range(1,10,lst)
+    multiple_element(lst)
+    pass
