e697a1a9dc13b2781a38c111fe3be4b7fc1673ae
mp-30 <adityabhadauriya93@gmail.com>
2025-04-07 12:27:22 +0530

    matrix started

---
+++ geekyforgeeky/Matrix/matrix1.py
@@ -0,0 +1,60 @@
+# Add two Matrices
+# Program to add two matrices using nested loop
+
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+
+
+def add_two(X,Y):
+    result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+    for i in range(0,len(X)):
+        for j in range(len(X[0])):
+        # for j in range(0,i+1):
+            result[i][j] = X[i][j] + Y[i][j]
+    print (result)
+
+
+
+
+
+
+
+
+
+
+# Multiply two matrices
+
+
+def mul_two(X,Y):
+    result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+    for i in range(0,len(X)):
+        for j in range(len(X[0])):
+        # for j in range(0,i+1):
+            result[i][j] = X[i][j] * Y[i][j]
+    print (result)
+
+# Matrix Product
+# Adding and Subtracting Matrices
+# Transpose a matrix in Single line
+# Matrix creation of n*n
+# Get Kth Column of Matrix
+# Vertical Concatenation in Matrix
+
+
+
+if __name__ == '__main__':
+    add_two(X,Y)
+    mul_two(X,Y)
+    pass
---
+++ geekyforgeeky/Matrix/matrix2.py
@@ -0,0 +1,12 @@
+from matrix1 import prin
+from matrix1 import prin2
+
+def hello(fu):
+    print(f'This is main {fu}')
+
+
+
+if __name__ == '__main__':
+
+
+    hello(123)
