fca84590277a54db0ea35915d29d0cfb7760fd42
MP-30 <adityabhadauriya93@gmail.com>
2025-05-01 15:23:20 +0530

    tuples

---
+++ geekyforgeeky/tuple/tuple.py
@@ -0,0 +1,13 @@
+# Find the size of a Tuple
+# Maximum and Minimum K elements in Tuple
+# Create a list of tuples from given list having number and its cube in each tuple
+# Adding Tuple to List and vice – versa
+# Closest Pair to Kth index element in Tuple
+# Join Tuples if similar initial element
+# Extract digits from Tuple list
+# All pair combinations of 2 tuples
+# Remove Tuples of Length K
+# Sort a list of tuples by second Item
+# Order Tuples using external List
+# Flatten tuple of List to tuple
+# Convert Nested Tuple to Custom Key Dictionary
