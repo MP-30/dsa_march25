645977b54558ca603f05d1d3a1d160e67c209630
mp-30 <adityabhadauriya93@gmail.com>
2025-09-14 17:28:15 +0530

    file handling started

---
+++ file_handling/opening_file.py
@@ -0,0 +1,3 @@
+file = open("file_handling/hello.txt","r")
+print(file.read())
+file.close()
--- udemy_das_course/linked_list/another_imple.py
+++ udemy_das_course/linked_list/another_imple.py
@@ -1,1 +1,29 @@
-print()
+class Node:
+     def __init__(self, data):
+          self.data = data
+          self.next = None
+          
+a = Node(3)
+b = Node(5)
+c = Node(8)
+
+head = a
+head.next = b
+b.next = c
+
+# print(head.data)
+# print(head.next.data)
+# print(head.next.next.data)
+
+# traverse linked list
+def printLinkedList():
+    curr = head 
+
+    while curr != None:
+        print(curr.data, end=' ')
+        curr = curr.next
+        
+printLinkedList()
+
+# insertion in linked list
+
