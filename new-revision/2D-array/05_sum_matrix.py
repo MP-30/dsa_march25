60ed57e10511089dbfbcb7ee5aebbf1c5f771a4e
mp-30 <adityabhadauriya93@gmail.com>
2026-08-30 06:33:56 +0530

    search and sum done

--- new-revision/2D-array/05_sum_matrix.py
+++ new-revision/2D-array/05_sum_matrix.py
@@ -1,19 +1,23 @@
 '''
 Given a 2D Matrix A of dimensions N*N, we need to return the sum of all possible submatrices.
 '''
-# A = [ [1, 2,3], [4, 5,6],[6,7,8] ]
-A = [[8,9,9,1,7],[5,5,10,1,0],[7,7,5,8,6],[7,3,7,9,2],[7,7,8,10,6]]
+# A = [ [1, 2,3], [4, 5,6],[7,8,9] ]
+A = [[8,9,9,1,7],
+     [5,5,10,1,0],
+     [7,7,5,8,6],
+     [7,3,7,9,2],
+     [7,7,8,10,6]]
 def solve(A):
-    summ = 0
-    sum_matrix = []
-    for i in range(len(A)):
-        for j in range(len(A)):
-            sum_matrix.append([i,j])
-    for a,b in sum_matrix:
-        for j in range(a,len(A)):
-            for k in range(b,len(A)):
-                summ += A[j][k]
-    return(summ)
+    n = len(A)
+    total = 0
+    for i in range(n):
+        for j in range(n):
+            count = (i +1) * (j +1) * (n-i) * ( n - j)
+            total += A[i][j] * count
+    return total
 
 
 print (solve(A))
+
+
+
